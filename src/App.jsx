3e2c56f94680998d537d34f2cd48323d394ea712
lozikk <sellars@lozikk.io>
2024-03-24 18:56:43 +0900

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import WelcomePage from './pages/WelcomePage'
+import WelcomePage from './pages/welcomePage'
 
 function App() {
   return <WelcomePage />
